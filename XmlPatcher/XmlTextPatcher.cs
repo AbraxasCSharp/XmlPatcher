2a2d1caf5cb3cdb384e3ac3347d8048da08db8f9
AbraxasCSharp <armin.kogler@hotmail.com>
2015-06-30 05:38:55 +0200

    Implemented one possible solution

--- XmlPatcher/XmlTextPatcher.cs
+++ XmlPatcher/XmlTextPatcher.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
 using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
 
 namespace XmlPatcher
 {
@@ -10,13 +12,50 @@ namespace XmlPatcher
 
     public XmlTextPatcher(string xmlText)
     {
-      _xDocument = new XmlDocument();
-      _xDocument.LoadXml(xmlText);
+      try
+      {
+        _xDocument = new XmlDocument();
+        _xDocument.LoadXml(xmlText);
+      }
+      catch (Exception ex)
+      {
+        throw new Exception("No valid xml string was given, see inner exception for more details.", ex);
+      }
+    }
+
+    private IXmlPatcher PatchAttribute(string xmlPath, string value)
+    {
+      var xAttribute = _xDocument.SelectSingleNode(xmlPath) as XmlAttribute;
+      if (xAttribute != null)
+      {
+        xAttribute.Value = value;
+      }
+
+      return this;
+    }
+
+    private IXmlPatcher PatchElement(string xmlPath, string value)
+    {
+      var xElement = _xDocument.SelectSingleNode(xmlPath) as XmlElement;
+      if (xElement != null)
+      {
+        xElement.InnerText = value;
+      }
+
+      return this;
     }
 
     public IXmlPatcher Patch(string xmlPath, string value)
     {
-      throw new NotImplementedException();
+      var xElement = _xDocument.SelectSingleNode(xmlPath) as XmlElement;
+      var xAttribute = _xDocument.SelectSingleNode(xmlPath) as XmlAttribute;
+
+      if (xElement != null)
+        return PatchElement(xmlPath, value);
+      if (xAttribute != null)
+        return PatchAttribute(xmlPath, value);
+
+      throw new Exception(string.Format("XPath '{0}' can't be found.", xmlPath));
     }
 
     public string Build()
